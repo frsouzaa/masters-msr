9c7a4dc8dcc3b0da4b6d97d9ce7defc14fd41349
frsouzaa <felipe.souza1506@outlook.com>
2026-09-16 00:40:35 -0300

    collect forks, prs and issues

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 import { GitHubApiRequest, GitHubApiClient, GitHubApiQueue } from "poolingh";
+import fs from "fs";
 
+const GH_API_BASE_URL = "https://api.github.com";
 const GH_TOKEN = process.env.GH_TOKEN;
-const REPO_NAME = "star-history/star-history";
+const REPO_NAME = "react/react";
 const STARS_API_PER_PAGE = 30; // https://docs.github.com/en/rest/activity/starring?apiVersion=2026-03-10#get-repository-star-history
+const FORKS_API_PER_PAGE = 100; // https://docs.github.com/en/rest/repos/forks?apiVersion=2026-03-10#list-forks
+const PULLS_API_PER_PAGE = 100; // https://docs.github.com/en/rest/pulls/pulls?apiVersion=2026-03-10#list-pull-requests
+const ISSUES_API_PER_PAGE = 100; // https://docs.github.com/en/rest/issues/issues?apiVersion=2026-03-10#list-repository-issues
 
-let client1 = new GitHubApiClient(GH_TOKEN, 10, 5000);
+const getApiClient = () => {
+  return new GitHubApiClient(GH_TOKEN, 10, 5000);
+}
 
-let queue = new GitHubApiQueue([client1]);
-
-const stars_per_day = [];
-
-function getWeeksBetween(date1, date2) {
+const getWeeksBetween = (date1, date2) => {
     // 1000ms * 60s * 60m * 24h * 7 days = milliseconds in one week
     const MS_IN_WEEK = 1000 * 60 * 60 * 24 * 7;
     // Get the absolute difference in milliseconds
@@ -19,40 +22,140 @@ function getWeeksBetween(date1, date2) {
     return Math.ceil(diffInMs / MS_IN_WEEK);
 }
 
-let request = new GitHubApiRequest(
-  `https://api.github.com/repos/${REPO_NAME}`,
-  {},
-  (result) => {
-    console.log(`Repositório ${REPO_NAME} encontrado com sucesso, data de criação: ${result.data.created_at}`);
-    const number_of_pages = Math.ceil(getWeeksBetween(new Date(result.data.created_at), new Date()) / STARS_API_PER_PAGE);
-    for (let i = 1; i <= number_of_pages; i++) {
-      let request = new GitHubApiRequest(
-        `https://api.github.com/repos/${REPO_NAME}/stargazers/history?per_page=${STARS_API_PER_PAGE}&page=${i}`,
-        {},
-        (result) => {
-          console.log(`Página ${i} processada com sucesso!`);
-          for (let j = 0; j < result.data.length; j++) {
-            for (let k = 0; k < result.data[j].days.length; k++) {
-              stars_per_day.push({
-                date: new Date((result.data[j].week + k * 24 * 60 * 60) * 1000), // convertendo para timestamp
-                count: result.data[j].days[k],
-              });
+const dumpVarIntoFile = (date, fileName) => {
+  fs.writeFile(`outputs/${fileName}`, JSON.stringify(date, null, 2), (err) => {
+      if (err) throw err;
+  });
+}
+
+const queueStarsPerDay = (queue) => {
+  const starsPerDay = [];
+  const request = new GitHubApiRequest(
+    `${GH_API_BASE_URL}/repos/${REPO_NAME}`,
+    {},
+    (result) => {
+      console.log(`Stars - Repositório ${REPO_NAME} encontrado com sucesso, data de criação: ${result.data.created_at}`);
+      const number_of_pages = Math.ceil(getWeeksBetween(new Date(result.data.created_at), new Date()) / STARS_API_PER_PAGE);
+      for (let i = 1; i <= number_of_pages; i++) {
+        const request = new GitHubApiRequest(
+          `${GH_API_BASE_URL}/repos/${REPO_NAME}/stargazers/history?per_page=${STARS_API_PER_PAGE}&page=${i}`,
+          {},
+          (result) => {
+            console.log(`Stars - Página ${i} processada com sucesso!`);
+            for (let j = 0; j < result.data.length; j++) {
+              for (let k = 0; k < result.data[j].days.length; k++) {
+                starsPerDay.push({
+                  date: new Date((result.data[j].week + k * 24 * 60 * 60) * 1000), // convertendo para timestamp
+                  count: result.data[j].days[k],
+                });
+              }
+            }
+            if (i === 1) {
+              console.log("Stars - Todas as páginas foram processadas com sucesso!");
+              dumpVarIntoFile(starsPerDay, "starsPerDay.json");
+              if (queue.getQueueLength() === 0) {
+                queue.stop();
+              }
             }
           }
-          if (i === 1) {
-            console.log("Todas as páginas foram processadas com sucesso!");
-            queue.stop();
-          }
-        }
-      );
-      queue.push(request);
+        );
+        queue.push(request);
+      }
     }
-  }
-)
+  )
+  queue.push(request);
+}
 
-queue.push(request);
+const queueForksPerDay = (queue) => {
+  const forksPerDay = [];
+  const request = new GitHubApiRequest(
+    `${GH_API_BASE_URL}/repos/${REPO_NAME}`,
+    {},
+    (result) => {
+      console.log(`Forks - Repositório ${REPO_NAME} encontrado com sucesso, quantidade de forks: ${result.data.forks_count}`);
+      const number_of_pages = Math.ceil(result.data.forks_count / FORKS_API_PER_PAGE);
+      for (let i = 1; i <= number_of_pages; i++) {
+        const request = new GitHubApiRequest(
+          `${GH_API_BASE_URL}/repos/${REPO_NAME}/forks?per_page=${FORKS_API_PER_PAGE}&page=${i}&sort=newest`,
+          {},
+          (result) => {
+            console.log(`Forks - Página ${i} processada com sucesso, quantidade de forks encontrados na página: ${result.data.length}`);
+            forksPerDay.push(...result.data.map(fork => ({
+              date: fork.created_at,
+              count: 1,
+            })));
+            if (i === 1) {
+              console.log("Forks - Todas as páginas foram processadas com sucesso!");
+              dumpVarIntoFile(forksPerDay, "forksPerDay.json");
+              if (queue.getQueueLength() === 0) {
+                queue.stop();
+              }
+            }
+          }
+        );
+        queue.push(request);
+      }
+    }
+  );
+  queue.push(request);
+}
 
-// últimos 4 anos ~= 210 semanas
-// paginação de 30 em 30, então 210 / 30 = 7 páginas
+const queuePullsPerDay = (queue, page=1, cache=[]) => {
+  const request = new GitHubApiRequest(
+    `${GH_API_BASE_URL}/repos/${REPO_NAME}/pulls?per_page=${PULLS_API_PER_PAGE}&page=${page}&state=all`,
+    {},
+    (result) => {
+      console.log(`Pulls - Página ${page} processada com sucesso, quantidade de pull requests encontrados na página: ${result.data.length}`);
+      cache.push(...result.data.map(pull => ({
+        html_url: pull.html_url,
+        created_at: pull.created_at,
+        closed_at: pull.closed_at,
+        merged_at: pull.merged_at,
+        count: 1,
+      })));
+      if (result.data.length < PULLS_API_PER_PAGE) {
+        dumpVarIntoFile(cache, "pullsPerDay.json");
+        if (queue.getQueueLength() === 0) {
+          queue.stop();
+        }
+        return;
+      }
+      queuePullsPerDay(queue, page + 1, cache);
+    }
+  );
+  queue.push(request);
+}
 
+const queueIssuesPerDay = (queue, page=1, cache=[]) => {
+  const request = new GitHubApiRequest(
+    `${GH_API_BASE_URL}/repos/${REPO_NAME}/issues?per_page=${ISSUES_API_PER_PAGE}&page=${page}&state=all`,
+    {},
+    (result) => {
+      console.log(`Issues - Página ${page} processada com sucesso, quantidade de issues encontrados na página: ${result.data.length}`);
+      cache.push(...result.data.filter(issue => issue.pull_request == null).map(issue => ({
+        html_url: issue.html_url,
+        created_at: issue.created_at,
+        closed_at: issue.closed_at,
+        merged_at: issue.merged_at,
+        count: 1,
+      })));
+      if (result.data.length < ISSUES_API_PER_PAGE) {
+        dumpVarIntoFile(cache, "issuesPerDay.json");
+        if (queue.getQueueLength() === 0) {
+          queue.stop();
+        }
+        return;
+      }
+      queueIssuesPerDay(queue, page + 1, cache);
+    }
+  );
+  queue.push(request);
+}
+
+let queue = new GitHubApiQueue([getApiClient()]);
+
+// queueStarsPerDay(queue);
+// queueForksPerDay(queue);
+// queuePullsPerDay(queue);
+queueIssuesPerDay(queue);
 queue.start();
